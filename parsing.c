f800ac14699678917fd5243067e52ce79bb8c576
Dmytro Oliferovych <dolifero@3-C-8.42heilbronn.de>
2024-06-03 19:13:57 +0200

    added correct prompt, fixed leaks

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:52:33 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/03 19:00:49 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:13:11 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,33 @@ void	leaks(void)
 	system("leaks minishell");
 }
 
+char	*get_prompt(void)
+{
+	char	*prompt;
+	char	*dir_name;
+	char	*prompt_no_arr;
+
+	dir_name = getcwd(NULL, 0);
+	prompt_no_arr = ft_strjoin("\033[1;97mminishell\033[0;39m", dir_name);
+	prompt = ft_strjoin(prompt_no_arr, "\033[1;36m > \033[0;39m");
+	free(prompt_no_arr);
+	free(dir_name);
+	return (prompt);
+}
+
 int	main(void)
 {
 	char	*input;
-	char	*dir_name;
 	char	*prompt;
 
-	// atexit(leaks);
+	atexit(leaks);
 	while (1)
 	{
-		dir_name = getcwd(NULL, 0);
-		prompt = ft_strjoin("\033[1;97mminishell\033[0;39m", dir_name);
-		prompt = ft_strjoin(prompt, "\033[1;36m > \033[0;39m");
-		// "\033[1;97mminishell > \033[0;39m"
+		prompt = get_prompt();
 		input = readline(prompt);
 		if (ft_strstr(input, "exit"))
 		{
+			free(prompt);
 			free(input);
 			exit (0);
 		}
