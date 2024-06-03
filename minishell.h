b0844cdcb2b720aab0f5ea814fe46a973a635103
Dmytro Oliferovych <dolifero@3-C-8.42heilbronn.de>
2024-06-03 16:56:51 +0200

    added header

---
+++ minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 16:54:47 by dolifero          #+#    #+#             */
+/*   Updated: 2024/06/03 16:56:14 by dolifero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "Libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
