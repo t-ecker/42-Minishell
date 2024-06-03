5939e5cc20d2c906ff115dc280b9e21019db9b8f
Dmytro Oliferovych <dolifero@3-C-8.42heilbronn.de>
2024-06-03 19:01:39 +0200

    added correct prompt

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:54:47 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/03 17:17:16 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/06/03 18:58:36 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,11 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef struct s_input
+{
+	char	*command;
+	char	*arguments;
+}				t_input;
 
 #endif
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:52:33 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/03 17:26:50 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:00:49 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,17 @@ void	leaks(void)
 int	main(void)
 {
 	char	*input;
+	char	*dir_name;
+	char	*prompt;
 
-	atexit(leaks);
+	// atexit(leaks);
 	while (1)
 	{
-		input = readline("\033[1;36mminishell > \033[0;39m");
+		dir_name = getcwd(NULL, 0);
+		prompt = ft_strjoin("\033[1;97mminishell\033[0;39m", dir_name);
+		prompt = ft_strjoin(prompt, "\033[1;36m > \033[0;39m");
+		// "\033[1;97mminishell > \033[0;39m"
+		input = readline(prompt);
 		if (ft_strstr(input, "exit"))
 		{
 			free(input);
