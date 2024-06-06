5da234b8a0fd2e60ac2bbe726fea7ec410d54668
Dmytro Oliferovych <dolifero@3-B-12.42heilbronn.de>
2024-06-06 19:10:31 +0200

    organized prompts

---
+++ prompts/prompt.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 16:52:33 by dolifero          #+#    #+#             */
+/*   Updated: 2024/06/06 18:36:14 by dolifero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char	*trim_dir_path(char *dir_path)
+{
+	int		i;
+	int		j;
+	char	*trimmed;
+
+	i = ft_strlen(dir_path);
+	while (i >= 0 && dir_path[i] != '/')
+		i--;
+	if (i >= 0)
+	{
+		trimmed = malloc(ft_strlen(dir_path) - i + 1);
+		if (!trimmed)
+			return (NULL);
+		j = 0;
+		while (dir_path[i + j] != '\0')
+		{
+			trimmed[j] = dir_path[i + j];
+			j++;
+		}
+		trimmed[j] = '\0';
+		return (trimmed);
+	}
+	else
+		return (dir_path);
+}
+
+char	*get_prompt(void)
+{
+	char	*prompt;
+	char	*dir_path;
+	char	*prompt_no_arr;
+
+	dir_path = getcwd(NULL, 0);
+	prompt = trim_dir_path(dir_path);
+	free(dir_path);
+	prompt_no_arr = ft_strjoin("\033[1;97mminishell \033[0;39m\033[2;96m",
+			prompt);
+	free(prompt);
+	prompt = ft_strjoin(prompt_no_arr, "\033[0;39m\033[1;36m > \033[0;39m");
+	free(prompt_no_arr);
+	return (prompt);
+}
