881865bbcd06285785ca6382f7ec63ec701b4704
Dmytro Oliferovych <dolifero@3-B-12.42heilbronn.de>
2024-06-06 19:09:59 +0200

    structurized parsing

---
+++ parsing/parsing.c
@@ -0,0 +1,156 @@
+#include "../includes/minishell.h"
+
+t_ast	*expr(int prec, t_token **token);
+
+t_ast	*create_ast_node(t_node_type type)
+{
+	t_ast	*node;
+
+	node = (t_ast *)malloc(sizeof(t_ast));
+	if (node)
+	{
+		node->type = type;
+		node->args = NULL;
+		node->filename = NULL;
+		node->heredoc = NULL;
+		node->left = NULL;
+		node->right = NULL;
+	}
+	return (node);
+}
+
+int	get_precedence(t_token_type type)
+{
+	if (type == T_IDENTIFIER)
+		return (0);
+	if (type == T_PIPE)
+		return (1);
+	if (type == T_GREAT || type == T_LESS || type == T_DGREAT
+		|| type == T_DLESS)
+		return (2);
+	if (type == T_AND || type == T_OR)
+		return (3);
+	if (type == T_OPAR || type == T_CPAR)
+		return (4);
+	printf("error precedence");
+	return (-1);
+}
+
+t_ast	*nud(t_token **token)
+{
+	int		i;
+	t_ast	*node;
+	t_token	*curr_token;
+	int		arg_count;
+
+	i = 0;
+	node = NULL;
+	if (*token)
+	{
+		if ((*token)->type == T_IDENTIFIER)
+		{
+			node = create_ast_node(N_COMMAND);
+			if (!node)
+				exit(1);
+			node->type = N_COMMAND;
+			curr_token = *token;
+			arg_count = 0;
+			while (curr_token && curr_token->type == T_IDENTIFIER)
+			{
+				arg_count++;
+				curr_token = curr_token->next;
+			}
+			node->args = malloc(sizeof(char *) * (arg_count + 1));
+			curr_token = *token;
+			while (i < arg_count)
+			{
+				node->args[i] = strdup(curr_token->value);
+				curr_token = curr_token->next;
+				i++;
+			}
+			node->args[arg_count] = NULL;
+			*token = curr_token;
+		}
+		else if ((*token)->type == T_OPAR)
+		{
+			(*token) = (*token)->next;
+			node = expr(3, token);
+			if ((*token) && (*token)->type == T_CPAR)
+				(*token) = (*token)->next;
+			else
+			{
+				printf("parenthesis do not close\n");
+				exit(1);
+			}
+		}
+	}
+	return (node);
+}
+
+void	create_note(t_token_type type, t_ast **node)
+{
+	if (type == T_PIPE)
+		*node = create_ast_node(N_PIPE);
+	else if (type == T_GREAT)
+		*node = create_ast_node(N_GREAT);
+	else if (type == T_LESS)
+		*node = create_ast_node(N_LESS);
+	else if (type == T_AND)
+		*node = create_ast_node(N_AND);
+	else if (type == T_OR)
+		*node = create_ast_node(N_OR);
+}
+
+t_ast	*led(t_ast *left, t_token **token)
+{
+	t_ast	*node;
+	int		prec;
+
+	node = NULL;
+	prec = get_precedence((*token)->type);
+	if (prec > 0)
+	{
+		create_note((*token)->type, &node);
+		node->left = left;
+		*token = (*token)->next;
+		if (prec == 2)
+		{
+			if ((*token)->type == T_IDENTIFIER)
+			{
+				node->filename = strdup((*token)->value);
+				(*token) = (*token)->next;
+			}
+			else
+			{
+				printf("missing file after redirection operator");
+				exit(1);
+			}
+		}
+		else
+			node->right = expr(prec, token);
+	}
+	return (node);
+}
+
+t_ast	*expr(int prec, t_token **token)
+{
+	t_ast	*left;
+
+	left = nud(token);
+	while (*token && get_precedence((*token)->type) <= prec)
+	{
+		if ((*token)->type == T_CPAR)
+			break ;
+		left = led(left, token);
+	}
+	return (left);
+}
+
+t_ast	*parse(t_token **token)
+{
+	t_ast	*node;
+
+	node = NULL;
+	node = expr(3, token);
+	return (node);
+}
---
+++ parsing/utils.c
@@ -0,0 +1,144 @@
+#include "../includes/minishell.h"
+
+const char	*node_type_to_string(t_node_type type)
+{
+	switch (type)
+	{
+	case N_COMMAND:
+		return ("N_COMMAND");
+	case N_LESS:
+		return ("N_LESS");
+	case N_GREAT:
+		return ("N_GREAT");
+	case N_DLESS:
+		return ("N_DLESS");
+	case N_DGREAT:
+		return ("N_DGREAT");
+	case N_PIPE:
+		return ("N_PIPE");
+	case N_AND:
+		return ("N_AND");
+	case N_OR:
+		return ("N_OR");
+	default:
+		return ("UNKNOWN");
+	}
+}
+
+void	print_ast_helper(t_ast *node, char *prefix)
+{
+	if (!node)
+		return ;
+	printf("%sNode Type: %s\n", prefix, node_type_to_string(node->type));
+	if (node->filename)
+	{
+		printf("%sFilename: %s\n", prefix, node->filename);
+	}
+	if (node->args)
+	{
+		printf("%sArguments: ", prefix);
+		for (int i = 0; node->args[i]; i++)
+		{
+			printf("%s ", node->args[i]);
+		}
+		printf("\n");
+	}
+	if (node->left)
+	{
+		printf("%sLeft:\n", prefix);
+		print_ast_helper(node->left, strcat(strcat(strdup(prefix), "  "),
+				"  "));
+	}
+	if (node->right)
+	{
+		printf("%sRight:\n", prefix);
+		print_ast_helper(node->right, strcat(strcat(strdup(prefix), "  "),
+				"  "));
+	}
+}
+
+void	print_ast(t_ast *node)
+{
+	print_ast_helper(node, "");
+	printf("\n");
+}
+
+void	free_ast(t_ast *node)
+{
+	if (!node)
+		return ;
+	if (node->type == N_COMMAND)
+	{
+		if (node->args)
+		{
+			for (int i = 0; node->args[i]; i++)
+			{
+				free(node->args[i]);
+			}
+			free(node->args);
+		}
+	}
+	else if (node->type == N_GREAT || node->type == N_LESS)
+	{
+		if (node->filename)
+			free(node->filename);
+	}
+	free_ast(node->left);
+	free_ast(node->right);
+	free(node);
+}
+
+void	free_tokens(t_token *token)
+{
+	t_token	*tmp;
+
+	while (token)
+	{
+		tmp = token;
+		token = token->next;
+		free(tmp->value);
+		tmp->value = NULL;
+		free(tmp); // Free the token after its value
+	}
+}
+
+const char	*token_type_to_string(t_token_type type)
+{
+	if (type == T_IDENTIFIER)
+		return ("T_IDENTIFIER");
+	else if (type == T_PIPE)
+		return ("T_PIPE	");
+	else if (type == T_GREAT)
+		return ("T_GREAT	");
+	else if (type == T_LESS)
+		return ("T_LESS	");
+	else if (type == T_DGREAT)
+		return ("T_DGREAT");
+	else if (type == T_DLESS)
+		return ("T_DLESS		");
+	else if (type == T_OR)
+		return ("T_OR	");
+	else if (type == T_AND)
+		return "T_AND	";
+	else if (type == T_OPAR)
+		return "T_OPAR	";
+	else if (type == T_CPAR)
+		return "T_CPAR	";
+	else
+		return "UNKNOWN";
+}
+
+void	print_token(t_token *token)
+{
+	int i = 1;
+	t_token *head = token;
+
+	while (head)
+	{
+		printf("\n");
+		printf("token %d, type:%s		value: %s", i,
+					token_type_to_string(head->type), head->value);
+		i++;
+		head = head->next;
+	}
+}
