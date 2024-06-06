5d1706fc68df23ddbc1e5a19983d47d19b6e1500
Dmytro Oliferovych <dolifero@3-B-12.42heilbronn.de>
2024-06-06 19:11:09 +0200

    added parsing functions to the main

--- minishell.c
+++ minishell.c
@@ -3,44 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tecker <tecker@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:54:07 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/06 17:33:02 by tecker           ###   ########.fr       */
+/*   Updated: 2024/06/06 18:24:13 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-void leaks(void)
+void	leaks(void)
 {
 	system("leaks minishell");
 }
 
-int main(void)
+int	main(void)
 {
-	char *input;
-	char *prompt;
+	char	*input;
+	char	*prompt;
+	t_token	*token;
+	t_token	*tmp;
+	t_token	*tmp2;
+	t_ast	*ast;
 
 	// atexit(leaks);
 	while (1)
 	{
 		prompt = get_prompt();
 		input = readline(prompt);
-
-		t_token *token = get_token(input);
-		t_token *tmp = token;
-		t_token *tmp2 = token;
-		t_ast *ast = parse(&token);
-
+		token = get_token(input);
+		tmp = token;
+		tmp2 = token;
+		ast = parse(&token);
 		print_token(tmp);
 		printf("\n\n");
 		print_ast(ast);
-
+		evaluate_ast(ast);
 		free_tokens(tmp2);
 		free_ast(ast);
-
-		ft_execute(input);
 		free(prompt);
 	}
 }
