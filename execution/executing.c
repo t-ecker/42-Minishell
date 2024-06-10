bf844b282fb011b87150254d19db7fa513625d68
Dmytro Oliferovych <dolifero@3-B-12.42heilbronn.de>
2024-06-10 17:27:53 +0200

    added a branch of an if nest to separate builtins from execv commands

--- execution/executing.c
+++ execution/executing.c
@@ -6,14 +6,15 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:09:27 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/06 18:24:04 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/06/10 17:15:21 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	evaluate_ast(t_ast *ast)
+void	evaluate_ast(t_shell *ms)
 {
-	if (ast->type == N_COMMAND)
-		ft_execute_command(ast->args);
+	if (ms->ast->type == N_COMMAND)
+		if (command_is_builtin(ms->ast->args))
+			ft_execute_builtin(ms);
 }
