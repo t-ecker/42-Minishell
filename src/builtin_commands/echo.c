6c7a753ac5b0019d3ed2876768fe55f0a15837a5
oliferovych <168459324+oliferovych@users.noreply.github.com>
2024-06-17 17:20:47 +0200

    added working unset, major changes to export

--- src/builtin_commands/echo.c
+++ src/builtin_commands/echo.c
@@ -3,19 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:53:39 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/11 17:29:33 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/06/17 17:16:04 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	ft_echo(char **arguments)
+void	ft_echo(t_ast *ast)
 {
-	if (strcmp(arguments[1], "-n") == 0)
-		ft_printf("%s", arguments[2]);
+	int		i;
+	char	*var;
+
+	if (strcmp(ast->args[1], "-n") == 0)
+		i = 2;
 	else
-		ft_printf("%s\n", arguments[1]);
+		i = 1;
+	while (ast->args[i] != NULL)
+	{
+		if (ast->args[i][0] == '$')
+		{
+			var = ft_strtrim(ast->args[i], "$");
+			if (variable_exists(ast->ms.env, var))
+				ft_printf("%s", ast->ms.env[variable_exists(ast->ms.env, var)]);
+			else
+				ft_printf("%s", ast->args[i]);
+		}
+		else
+			ft_printf("%s", ast->args[i]);
+		i++;
+	}
+	write(1, "\n", 1);
 }
--- src/builtin_commands/execute_builtin.c
+++ src/builtin_commands/execute_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:59:59 by dolifero          #+#    #+#             */
-/*   Updated: 2024/06/14 20:26:30 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/06/17 17:04:24 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,13 @@ void	ft_execute_builtin(t_ast *ast, int builtin)
 	else if (builtin == PWD)
 		ft_pwd();
 	else if (builtin == ECHO)
-		ft_echo(ast->args);
+		ft_echo(ast);
 	else if (builtin == ENV)
 		ft_env(ast);
-
-	// else if (strcmp(arguments[0], "exit") == 0)
-	// else if (strcmp(arguments[0], "exit") == 0)
+	else if (builtin == UNSET)
+		ft_unset(ast);
+	else if (builtin == EXPORT)
+		ft_exp(ast);
 	// else if (strcmp(arguments[0], "exit") == 0)
 	// else if (strcmp(arguments[0], "exit") == 0)
 	// else if (strcmp(arguments[0], "exit") == 0)
