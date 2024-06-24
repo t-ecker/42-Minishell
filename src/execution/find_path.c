91d5593925a8a1a153364e5f8eb7a84a27425fe5
oliferovych <168459324+oliferovych@users.noreply.github.com>
2024-06-24 13:56:57 +0200

    fixed path finding, made work with absolute path

--- src/execution/find_path.c
+++ src/execution/find_path.c
@@ -10,40 +10,63 @@ void	ft_errorcode_exit(char *message, int code, t_ast *ast)
 	ft_printf("%s\n", var);
 	free(char_code);
 	if (message != NULL)
-		perror(ast->args[0]);
+		perror(message);
 	ft_change_existing(var, ast->ms.env);
 	free(var);
 	exit(code);
 }
 
-void	ft_execvp(t_ast *ast)
+char	*ft_find_path(t_ast *ast)
 {
-	char *path;
+	char	*path;
+
+	if (ft_strncmp(ast->args[0], "./", 2) == 0 || ft_strncmp(ast->args[0], "/", 1) == 0
+		|| ft_strncmp(ast->args[0], "../", 3) == 0 || access(ast->args[0], F_OK) == 0)
+		path = ft_strdup(ast->args[0]);
+	else
+		path = ft_strdup(ast->ms.env[variable_exists(ast->ms.env, "PATH")] + 5);
+	return (path);
+}
+
+void	case_split_path(char *path, t_ast *ast)
+{
 	char **spl_path;
 	char *full_path;
 	int i;
 
-	if (variable_exists(ast->ms.env, "PATH") || variable_exists(ast->ms.env, "PATH") == 0)
+	spl_path = ft_split(path, ':');
+	free(path);
+	i = 0;
+	while (spl_path[i] != NULL)
 	{
-		path = ft_strdup(ast->ms.env[variable_exists(ast->ms.env, "PATH")] + 5);
-		spl_path = ft_split(path, ':');
-		free(path);
-		i = 0;
-		while (spl_path[i] != NULL)
+		full_path = malloc(strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
+		ft_strlcpy(full_path, spl_path[i], strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
+		ft_strlcat(full_path, "/", strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
+		ft_strlcat(full_path, ast->args[0], strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
+		if (execve(full_path, ast->args, ast->ms.env) == -1)
 		{
-			full_path = malloc(strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
-			ft_strlcpy(full_path, spl_path[i], strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
-			ft_strlcat(full_path, "/", strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
-			ft_strlcat(full_path, ast->args[0], strlen(spl_path[i]) + strlen(ast->args[0]) + 2);
-			if (execve(full_path, ast->args, ast->ms.env) == -1)
-			{
-				free(full_path);
-				if (spl_path[i + 1] == NULL)
-					ft_errorcode_exit("Command not found.", 127, ast);
-			}
-			i++;
+			free(full_path);
+			if (spl_path[i + 1] == NULL)
+				ft_errorcode_exit(ast->args[0], 127, ast);
 		}
-		ft_errorcode_exit(NULL, 0, ast);
+		i++;
 	}
-	return (ft_errorcode_exit("Path is not set.", 127, ast));
+}
+
+void	ft_execvp(t_ast *ast)
+{
+	char *path;
+
+	path = ft_find_path(ast);
+	if (ft_strchr(path, ':') && variable_exists(ast->ms.env, "PATH") >= 0)
+		case_split_path(path, ast);
+	else
+	{
+		if (execve(path, ast->args, ast->ms.env) == -1)
+		{
+			free(path);
+			ft_errorcode_exit(ast->args[0], 127, ast);
+		}
+	}
+	ft_errorcode_exit(NULL, 0, ast);
 }
