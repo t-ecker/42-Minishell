04a26616c1b28275b1982edeab85f335cb097ffd
oliferovych <168459324+oliferovych@users.noreply.github.com>
2024-07-14 14:34:27 +0200

    final echo & export

--- src/builtin_commands/echo.c
+++ src/builtin_commands/echo.c
@@ -3,39 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:53:39 by dolifero          #+#    #+#             */
-/*   Updated: 2024/07/11 21:25:24 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/07/14 14:30:03 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static void	echo_it(t_ast *ast, int i)
+{
+	while (ast->args[i])
+	{
+		if (!(ast->args[i][0] == '$' && ast->args[i][1])
+			|| ast->tran[i][0] == 0)
+			ft_printf("%s", ast->args[i]);
+		if (ast->args[i + 1])
+			ft_printf(" ");
+		i++;
+	}
+}
+
 void	ft_echo(t_ast *ast)
 {
 	int	i;
+	int	j;
 	int	flag;
 
 	i = 1;
 	flag = 0;
-	if (ast->args[i] && ft_strnstr(ast->args[i], "-n", 2) != NULL)
+	while (ast->args[i] && ast->args[i][0] == '-')
 	{
-		flag++;
+		j = 1;
+		while (ast->args[i][j] == 'n')
+			j++;
+		if (ast->args[i][j] == '\0' && ast->args[i][j - 1] == 'n')
+			flag++;
+		else
+			break ;
 		i++;
 	}
 	if (ast->args[i])
-	{
-		while (ast->args[i])
-		{
-			if (!(ast->args[i][0] == '$' && ast->args[i][1])
-				|| ast->tran[i][0] == 0)
-				ft_printf("%s", ast->args[i]);
-			if (ast->args[i + 1])
-				ft_printf(" ");
-			i++;
-		}
-	}
+		echo_it(ast, i);
 	if (flag == 0)
 		ft_printf("\n");
 }
